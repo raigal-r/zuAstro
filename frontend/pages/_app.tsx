e2d052287413fc9786201e9bc67dc813a1427e15
joyjsmun <joy.js.mun@gmail.com>
2023-11-09 21:13:59 +0300

    ADD: after merging login

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
@@ -9,16 +8,4 @@ export default function App({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
     </Layout>
   );
-=======
-import '@/styles/globals.css'
-import { useRouter } from 'next/router'
-import type { AppProps } from 'next/app'
-import { ZupassPopupRedirect } from './login';
-
-
-export default function App({ Component, pageProps }: AppProps) {
-
-  return <Component {...pageProps} />
-  
->>>>>>> popup
 }
