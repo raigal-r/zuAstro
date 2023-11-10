6a84795c32c7e99551ed2c1baa5d2563d260c0b3
raigal-r <raquel.raigal@outlook.es>
2023-11-10 14:59:22 +0300

    build ok

--- frontend/pages/test.tsx
+++ frontend/pages/test.tsx
@@ -66,3 +66,15 @@ export function InputForm() {
     </Form>
   );
 }
+
+import React from 'react';
+
+const TestPage = () => {
+  return (
+    <div>
+      <h1>This is a test page</h1>
+    </div>
+  );
+};
+
+export default TestPage;
