23ee87245f6e36c56ae7e8bcf59dca20deecabc1
joyjsmun <joy.js.mun@gmail.com>
2023-11-11 04:29:43 +0300

    ADD: NFC card page

--- frontend/pages/createNFC.tsx
+++ frontend/pages/createNFC.tsx
@@ -1,35 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Inter } from "next/font/google";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import router from "next/router";
+import TextInput from "@/components/TextInput";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function CreateNFC() {
+export default function DailyHoroscope() {
+  const [nickName, setNickName] = useState("");
+  const [day, setDay] = useState("");
+  const [month, setMonth] = useState("");
+  const [year, setYear] = useState("");
+  const [hour, setHour] = useState("");
+
+  const handleInputChange = (
+    param: "day" | "month" | "year" | "hour",
+
+    newValue: string
+  ) => {
+    if (param === "day") {
+      setDay(newValue);
+    } else if (param === "month") {
+      setMonth(newValue);
+    } else if (param === "year") {
+      setYear(newValue);
+    } else if (param === "hour") {
+      setHour(newValue);
+    }
+  };
   return (
-    <section className="h-[100vh] w-full flex justify-center ">
-      <div className="flex-col w-full">
-        <p className="text-3xl mb-2 text-gray-600 text-left ">
-          Create Your NFC
-        </p>
+    <section className="h-[100vh] w-full flex justify-center gap-10 ">
+      <div className="flex-col w-full h-full items-start ">
+        <div className="flex-col mb-7">
+          <h1 className="text-3xl mb-2 text-gray-600">Create Your NFC Card</h1>
+        </div>
+        <div>
+          <TextInput
+            label="Nick Name"
+            value={day}
+            onChange={(value) => handleInputChange("day", value)}
+          />
+        </div>
+        <div>
+          <TextInput
+            label="Telegram Handle"
+            value={day}
+            onChange={(value) => handleInputChange("day", value)}
+          />
+        </div>
+        <div>
+          <TextInput
+            label="Twitter Handle"
+            value={day}
+            onChange={(value) => handleInputChange("day", value)}
+          />
+        </div>
+        <div className="flex justify-center items-center w-full gap-2">
+          <button
+            className="bg-aGreen text-white font-medium text-xl py-3 mt-4 px-4 text-center"
+            onClick={() => {
+              router.push("./");
 
-        {/* need to fix layout */}
-        <form className="flex-col w-full">
-          <label> Nick Name</label>
-          <input placeholder="Nick Name" />
-          <label>Telegram Handle</label>
-          <input placeholder="Telegram Handle" />
-          <label>Twitter Handle</label>
-          <input placeholder="Twitter Handle" />
-        </form>
-
-        <div className="flex justify-center items-center">
-          <Link href="/">
-            <button className="bg-aGreen text-white font-medium text-xl py-3 mt-4 w-44  text-center">
-              Create NFC
-            </button>
-          </Link>
+              //logInContext.logInTheme = true
+            }}
+          >
+            Create NFC Card
+          </button>
         </div>
       </div>
     </section>
